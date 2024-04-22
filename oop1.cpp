d5af06360b248c2bb76e85a631ce69ef40a6c020
mhmdfakhrim <muhfakhri4@gmail.com>
2024-04-22 16:09:18 +0700

    commit

--- oop1.cpp
+++ oop1.cpp
@@ -28,5 +28,11 @@ class Matakuliah{
             cin >> namaMK;
             cout << "SKS: ";
             cin >> sks;
-        }      
+        }
+  void output(){
+
+        cout << "Kode MK: " << kodeMK << endl;
+        cout << "Nama MK: " << namaMK << endl;
+        cout << "SKS: " << sks << endl;
+      }            
 };
