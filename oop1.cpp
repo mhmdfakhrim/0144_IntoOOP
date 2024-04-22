7ef53fecee6b0d799e3e5ebaaa5e9a460ae522de
mhmdfakhrim <muhfakhri4@gmail.com>
2024-04-22 16:03:37 +0700

    commit class mahasiswa

--- oop1.cpp
+++ oop1.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+
+class Mahasiswa{
+    public: //akses modifier
+    string nama;
+    int umur;
+    string jurusan;
+
+    void output(){
+        cout << "Nama: " << nama << endl;
+        cout << "Umur: " << umur << endl;
+        cout << "Jurusan: " << jurusan << endl;
+    }
+
+};
