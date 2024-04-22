6071735daaa3db84bf1fa926247a6d90db578186
mhmdfakhrim <muhfakhri4@gmail.com>
2024-04-22 16:24:28 +0700

    commit

--- oop2.cpp
+++ oop2.cpp
@@ -18,7 +18,13 @@ class bangunDatar{
     }
 
     float hitungLuas (){
-      
+
         return panjang * lebar;
     }    
+
+    void display(){
+        cout << "Panjangnya = " << panjang << endl;
+        cout << "Lebarnya = " << lebar << endl;
+        cout << "Luasnya = " << hitungLuas() << endl; 
+    }
 };
